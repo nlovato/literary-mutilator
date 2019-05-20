c0768ee0a08a727f998a576977489d194e989fe6
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-19 19:45:32 -0600

    added javascript font color and font style.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //Change font color
-document.getElementById("one").style.color ="orange";
+document.getElementById("one").style.color ="#5b5fff";
+document.getElementById("two").style.color ="#df80ff";
+document.getElementById("three").style.color ="#eb5368";
+
+//change font style
+document.getElementById("one").style.fontFamily = "fantasy";
+document.getElementById("two").style.fontFamily = "cursive";
