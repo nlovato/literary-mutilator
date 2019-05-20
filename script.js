2a49827c1be9f9c17c22f23e46c7eea5fbb1b723
Natasha Lovato <nmarshlovato@cnm.edu>
2019-05-19 20:27:55 -0600

    added cbackground and buttons

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.getElementById("three").style.color ="#eb5368";
 //change font style
 document.getElementById("one").style.fontFamily = "fantasy";
 document.getElementById("two").style.fontFamily = "cursive";
+document.getElementById("three").style.fontFamily = "monospace";
+
+//change background color
+document.getElementById("one").style.backgroundColor = "#ffffb4";
+document.getElementById("two").style.backgroundColor = "#ffffb4";
+document.getElementById("three").style.backgroundColor = "#ffffb4";
+
+//replace and switch taco with yum
+function () {
+	let tacoPara = document.getElementById("three");
+	let regex = /taco/;
+	tacoPara.innerHTML = tacoPara.innerHTML.replace(regex, 'YUM')
+}
